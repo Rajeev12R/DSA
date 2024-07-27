230ad41b39db5d50adecf1698a9691ff67c65dc9
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-07-27 23:03:32 +0530

    Basic function

---
+++ Leetcode/sum_function.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sum(int m, int n)
+{
+    int c = m + n;
+    return c;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << sum(a, b);
+    return 0;
+}
