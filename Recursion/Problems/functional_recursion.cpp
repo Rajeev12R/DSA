2d1ee65b476fd249c42e620a795a330e76a389ca
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-08-21 16:30:43 +0530

    Funstional recursion

---
+++ Recursion/Problems/functional_recursion.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+void f(int i, int sum)
+{
+    if (i < 0)
+    {
+        cout << sum;
+        return;
+    }
+    f(i - 1, sum + i);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    f(n, 0);
+    return 0;
+}
