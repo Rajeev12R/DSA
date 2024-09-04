36f5af74dcd98dd7a010f28671a8b8f9fce65f4d
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-09-04 18:57:48 +0530

    Array problem

--- Array/largest_element.cpp
+++ Array/largest_element.cpp
@@ -1,5 +1,5 @@
 // Brute force (using Bubble sort)
-
+/*
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -16,6 +16,47 @@ for(int i = n-1; i >=0 ; i--){
         }
     }
 }
-cout << arr[n-2];
+if(arr[n-2] == arr[n-1]){
+cout << arr[n-3];
+
+}
+else{
+    cout << arr[n-2] ;
+}
 return 0;
+}
+
+*/
+// Optimal Case
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int largest = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    int secondlargest = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != largest && arr[i] > secondlargest)
+        {
+            secondlargest = arr[i];
+        }
+    }
+    cout << secondlargest;
+
+    return 0;
 }
