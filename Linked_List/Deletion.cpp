8a97b0404838eb22ba18111f50a265913bbf2823
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-09-11 21:29:07 +0530

    K position deletion

--- Linked_List/Deletion.cpp
+++ Linked_List/Deletion.cpp
@@ -72,7 +72,6 @@ int main()
     return 0;
 }
 
-*/
 
 // Tail Deletion
 #include <bits/stdc++.h>
@@ -155,6 +154,107 @@ int main()
         head = insert(head, val);
     }
     head = deleteTail(head);
+    printAll(head);
+
+    return 0;
+}
+*/
+
+// Kth position deletion
+
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+
+public:
+    node(int data1, node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+public:
+    node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+node *insert(node *head, int val)
+{
+    node *newE = new node(val);
+    if (head == nullptr)
+    {
+        head = newE;
+        return head;
+    }
+    node *temp = head;
+    while (temp->next)
+    {
+        temp = temp->next;
+    }
+    temp->next = newE;
+    return head;
+}
+node *deletek(node *head, int k)
+{
+    if (head == nullptr)
+    {
+        return head;
+    }
+    if (k == 1)
+    {
+        node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+    int cnt = 0;
+    node *temp = head;
+    node *prev = nullptr;
+    while (temp != nullptr)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+void printAll(node *head)
+{
+    node *temp = head;
+    while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    node *head = nullptr;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        head = insert(head, val);
+    }
+    int k;
+    cin >> k;
+
+    head = deletek(head, k);
     printAll(head);
 
     return 0;
