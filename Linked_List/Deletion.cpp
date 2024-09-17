aad9edf1571ffe99f554059cc84ba9f03fd3edf2
Rajeev Ranjan <rjranjan2112@gmail.com>
2024-09-17 22:56:12 +0530

    Second Last element deletion

--- Linked_List/Deletion.cpp
+++ Linked_List/Deletion.cpp
@@ -263,6 +263,101 @@ int main()
 
 // Element Deletion
 
+// #include <bits/stdc++.h>
+// using namespace std;
+// class node
+// {
+// public:
+//     int data;
+//     node *next;
+
+// public:
+//     node(int data1, node *next1)
+//     {
+//         data = data1;
+//         next = next1;
+//     }
+
+// public:
+//     node(int data1)
+//     {
+//         data = data1;
+//         next = nullptr;
+//     }
+// };
+
+// node *insert(node *head, int val)
+// {
+//     node *newE = new node(val);
+//     if (head == nullptr)
+//     {
+//         head = newE;
+//         return head;
+//     }
+//     node *temp = head;
+//     while (temp->next)
+//     {
+//         temp = temp->next;
+//     }
+//     temp->next = newE;
+//     return head;
+// }
+// node *deletek(node *head, int el)
+// {
+//     if (head == NULL)
+//     {
+//         return head;
+//     }
+//     if (head->data == el)
+//     {
+//         node *temp = head;
+//         head = head->next;
+//         delete temp;
+//         return head;
+//     }
+//     node *temp = head;
+//     node *prev = nullptr;
+//     while (temp != nullptr)
+//     {
+//         if (temp->data == el)
+//         {
+//             prev->next = prev->next->next;
+//             delete temp;
+//             break;
+//         }
+//         prev = temp;
+//         temp = temp->next;
+//     }
+//     return head;
+// }
+// void printAll(node *head)
+// {
+//     node *temp = head;
+//     while (temp)
+//     {
+//         cout << temp->data << " ";
+//         temp = temp->next;
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     node *head = nullptr;
+//     for (int i = 0; i < n; i++)
+//     {
+//         int val;
+//         cin >> val;
+//         head = insert(head, val);
+//     }
+//     int el;
+//     cin >> el;
+
+//     head = deletek(head, el);
+//     printAll(head);
+
+//     return 0;
+// }
 #include <bits/stdc++.h>
 using namespace std;
 class node
@@ -285,14 +380,12 @@ public:
         next = nullptr;
     }
 };
-
 node *insert(node *head, int val)
 {
     node *newE = new node(val);
     if (head == nullptr)
     {
-        head = newE;
-        return head;
+        return newE;
     }
     node *temp = head;
     while (temp->next)
@@ -302,32 +395,30 @@ node *insert(node *head, int val)
     temp->next = newE;
     return head;
 }
-node *deletek(node *head, int el)
+node *deleteSecond(node *head)
 {
-    if (head == NULL)
+    if (head == nullptr || head->next == nullptr)
     {
-        return head;
+        return nullptr;
     }
-    if (head->data == el)
+    if (head->next->next == nullptr)
     {
         node *temp = head;
         head = head->next;
         delete temp;
         return head;
     }
-    node *temp = head;
     node *prev = nullptr;
-    while (temp != nullptr)
+    node *temp = head;
+    while (temp->next->next)
     {
-        if (temp->data == el)
-        {
-            prev->next = prev->next->next;
-            delete temp;
-            break;
-        }
         prev = temp;
         temp = temp->next;
     }
+    node* second = temp;
+    temp= temp->next;
+    prev->next = temp;
+    delete second;
     return head;
 }
 void printAll(node *head)
@@ -350,11 +441,10 @@ int main()
         cin >> val;
         head = insert(head, val);
     }
-    int el;
-    cin >> el;
-
-    head = deletek(head, el);
+    cout << "Original Linked List: ";
     printAll(head);
-
-    return 0;
+    head = deleteSecond(head);
+    cout << endl;
+    cout << "Updated Linked List: ";
+    printAll(head);
 }
